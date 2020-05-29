f2a70b6790e6e7017423eebdd374d4bb330a78a7
kou <kou.pass94@gmail.com>
2020-05-29 20:37:30 +0300

    Added Flight menu callback

---
+++ BookingService.TgBot/src/Callbacks/FlightsMenuCallback.cs
@@ -0,0 +1,188 @@
+using System.Linq;
+using System.Threading.Tasks;
+using BookingService.Client;
+using BookingService.TgBot.StateMachine;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace BookingService.TgBot.Callbacks
+{
+    public sealed class FlightsMenuCallback : Callback
+    {
+        public override string Query => "flightsMenu|fromCountry|toCountry";
+
+        public override async Task Execute(Message message, TelegramBotClient client, User from)
+        {
+            string answer = "";
+            IFlight ifl = new ApiClientWrapper(AppSettings.GetEntry("URL"));
+
+
+            if (Bot.UserStates[message.Chat.Id].CurrentState == UserState.InMainMenu)
+                Bot.UserStates[message.Chat.Id].SetState(UserState.InFlightsMenu);
+            Logger.GetState(message.Chat.Username, Bot.UserStates[message.Chat.Id]);
+
+            if (Bot.UserStates[message.Chat.Id].CurrentState == UserState.InFlightsMenu)
+            {
+                answer = "Find the country you're going...";
+                await client.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: answer,
+                    replyMarkup: new InlineKeyboardMarkup(new []
+                    {
+                        new []
+                        {
+                            InlineKeyboardButton.WithCallbackData("From", "fromCountry")    // TODO: change name
+                        },
+                        new []
+                        {
+                            InlineKeyboardButton.WithCallbackData("To", "toCountry")        // TODO: change name
+                        },
+                        new []
+                        {
+                            InlineKeyboardButton.WithCallbackData("Back to menu", "menu")
+                        }
+                    })
+                );
+            }
+            else if (Bot.UserStates[message.Chat.Id].CurrentState == UserState.DepartureCountryInputStarted)
+            {
+                answer = "Please, reply on this message with name country of departure:";
+                await client.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: answer,
+                    replyMarkup: new ForceReplyMarkup()
+                );
+
+                Bot.UserStates[message.Chat.Id].SetState(StateMachine.UserState.DepartureCountryInputEnded);
+            }
+            else if (Bot.UserStates[message.Chat.Id].CurrentState == UserState.DepartureCountryInputEnded)
+            {
+                // Command logic
+                var flights = (await ifl.GetFlightsAsync())
+                    .Where(f => f.From.Name == message.Text)
+                    .ToList();
+
+                // Show all found flights
+                if (flights == null)
+                {
+                    await client.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: "No flights from this country yet. Sorry for inconvenience.",
+                        replyMarkup: new InlineKeyboardMarkup(new []
+                        {
+                            new []
+                            {
+                                InlineKeyboardButton.WithCallbackData("Back to menu", "menu")
+                            }
+                        })
+                    );
+                }
+                else
+                {
+                    await client.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: $"Found {flights.Count} flights"
+                    );
+                    foreach (var flight in flights)
+                        await client.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: 
+                            $"Id: {flight.Id}\n" +
+                            $"From: {flight.From.Name}\n" + 
+                            $"To: {flight.To.Name}",
+                        replyMarkup: new InlineKeyboardMarkup(new []
+                        {
+                            new []
+                            {
+                                InlineKeyboardButton.WithCallbackData("Create reservation", "createReservation")
+                            }
+                        }) 
+                    );
+                }
+
+                // Print message with button to return to main menu
+                await client.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "Choose an action:",
+                    replyMarkup: new InlineKeyboardMarkup(new []
+                    {
+                        new []
+                        {
+                            InlineKeyboardButton.WithCallbackData("Back to menu", "menu")
+                        }
+                    })
+                );
+            }
+            else if (Bot.UserStates[message.Chat.Id].CurrentState == UserState.ArrivalCountryInputStarted)
+            {
+                answer = "Please, reply on this message with name country of arrival:";
+                await client.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: answer,
+                    replyMarkup: new ForceReplyMarkup()
+                );
+
+                Bot.UserStates[message.Chat.Id].SetState(StateMachine.UserState.ArrivalCountryInputEnded);
+            }
+            else if (Bot.UserStates[message.Chat.Id].CurrentState == UserState.ArrivalCountryInputEnded)
+            {
+                // Command logic
+                var flights = (await ifl.GetFlightsAsync())
+                    .Where(f => f.To.Name == message.Text)
+                    .ToList();
+
+                // Show all found flights
+                if (flights == null)
+                {
+                    await client.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: "No flights to this country yet. Sorry for inconvenience.",
+                        replyMarkup: new InlineKeyboardMarkup(new []
+                        {
+                            new []
+                            {
+                                InlineKeyboardButton.WithCallbackData("Back to menu", "menu")
+                            }
+                        })
+                    );
+                }
+                else
+                {
+                    await client.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: $"Found {flights.Count} flights"
+                    );
+                    foreach (var flight in flights)
+                        await client.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: 
+                            $"Id: {flight.Id}\n" +
+                            $"From: {flight.From.Name}\n" + 
+                            $"To: {flight.To.Name}",
+                        replyMarkup: new InlineKeyboardMarkup(new []
+                        {
+                            new []
+                            {
+                                InlineKeyboardButton.WithCallbackData("Create reservation", "createReservation")
+                            }
+                        }) 
+                    );
+                }
+
+                // Print message with button to return to main menu
+                await client.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "Choose an action:",
+                    replyMarkup: new InlineKeyboardMarkup(new []
+                    {
+                        new []
+                        {
+                            InlineKeyboardButton.WithCallbackData("Back to menu", "menu")
+                        }
+                    })
+                );
+            }
+        }
+    }
+}
